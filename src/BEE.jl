b89dc88c587d5529a51134934d7fb52958c4e231
Xing Shi Cai <newptcai@gmail.com>
2020-04-19 23:01:20 +0200

    adddiff done

--- src/BEE.jl
+++ src/BEE.jl
@@ -2,7 +2,8 @@ module BEE
 
 import Base: -, +, *, /, mod, max, min, ==, <, <=, !=, >, >=, sum, show, convert
 
-export BeeInt, BeeBool, BeeModel, @beeint, @beebool, beeint, beebool, render, reset
+export BeeInt, BeeBool, BeeModel, 
+    @beeint, @beebool, beeint, beebool, render, reset, and, or, iff, alldiff
 
 # -------------------------------------------------------------
 # abstract types
@@ -264,9 +265,9 @@ for (ET, EF, OP) in intBOP
     end
 
     # `lhs` is true `iff` rhs is true
-    (==)(lhs::BeeBoolLiteral, rhs::$ET) = rhs == lhs
-    function (==)(lhs::$ET, rhs::BeeBoolLiteral)
-        if rhs.val
+    (==)(lhs::Bool, rhs::$ET) = rhs == lhs
+    function (==)(lhs::$ET, rhs::Bool)
+        if rhs
             $(Symbol(:int_, EF))(lhs.lhs, lhs.rhs)
         else
             $(Symbol(:int_, EF, :_reif))(lhs.lhs, lhs.rhs, rhs)
@@ -284,6 +285,7 @@ for (ET, EF, OP) in  intAOP
     end
 
     # `lhs` == `rhs` is true
+    (==)(lhs::BeeInteger, rhs::$ET) = rhs == lhs
     function (==)(lhs::$ET, rhs::BeeInteger)
         $(Symbol(:int_, EF))(lhs.lhs, lhs.rhs, rhs)
     end
@@ -291,8 +293,8 @@ for (ET, EF, OP) in  intAOP
 end
 
 # Create BEE operations on one integer array
-intarrayOP = [(:BeeAllDiff, :allDiff, :alldiff)]
-for (ET, EF, OP) in intarrayOP
+intarrayOP = [(:BeeArrayAllDiff, :allDiff, :alldiff, :_reif)]
+for (ET, EF, OP, TAIL) in intarrayOP
     @eval BEE begin
     struct $ET <: BeeExpression
         varlist::Array{BeeInteger, 1}
@@ -304,22 +306,59 @@ for (ET, EF, OP) in intarrayOP
     end
 
     # No need to check type here
-    $OP(varlist) = $ET(varlist)
+    $OP(varlist::Array{T, 1} where T <: ZZ) = $ET(varlist)
 
-    (==)(lhs::BeeBoolean, rhs::$ET) = rhs == lhs
-    function (==)(lhs::$ET, rhs::BeeBoolean)
-        $(Symbol(:bool_array_, EF, :_reif))(lhs.varlist, rhs)
+    (==)(lhs::BeeInteger, rhs::$ET) = rhs == lhs
+    function (==)(lhs::$ET, rhs::BeeInteger)
+        $(Symbol(:int_array_, EF, TAIL))(lhs.varlist, rhs)
     end
 
+    (==)(lhs::Bool, rhs::$ET) = rhs == lhs
     function (==)(lhs::$ET, rhs::Bool)
         if rhs
             $(Symbol(:int_array_, EF))(lhs.varlist)
         else
-            $(Symbol(:int_array_, EF, :_reif))(lhs.varlist, rhs)
+            $(Symbol(:int_array_, EF, TAIL))(lhs.varlist, rhs)
         end
     end
     end
 end
+
+# Create BEE operations on one integer array
+#intarrayOP = [(:BeeArrayAllDiff, :allDiff, :alldiff, :_reif),
+#              (:BeeArrayPlus, :plus, :plus, :()),
+#              (:BeeArrayTimes, :times, :times, :()), 
+#              (:BeeArrayMax, :max, :max, :()), 
+#              (:BeeArrayMin, :min, :min, :())]
+#for (ET, EF, OP, TAIL) in intarrayOP
+#    @eval BEE begin
+#    struct $ET <: BeeExpression
+#        varlist::Array{BeeInteger, 1}
+#
+#        # Convert everything in the list to BeeBoolean
+#        # function $ET(varlist::Array{T, 1} where T <: ZZ)
+#        #     new([convert(BeeInteger, v) for v in varlist])
+#        # end
+#    end
+#
+#    # No need to check type here
+#    $OP(varlist::Array{T, 1} where T <: ZZ) = $ET(varlist)
+#
+#    (==)(lhs::BeeInteger, rhs::$ET) = rhs == lhs
+#    function (==)(lhs::$ET, rhs::BeeInteger)
+#        $(Symbol(:int_array_, EF, TAIL))(lhs.varlist, rhs)
+#    end
+#
+#    (==)(lhs::Bool, rhs::$ET) = rhs == lhs
+#    function (==)(lhs::$ET, rhs::Bool)
+#        if rhs
+#            $(Symbol(:int_array_, EF))(lhs.varlist)
+#        else
+#            $(Symbol(:int_array_, EF, TAIL))(lhs.varlist, rhs)
+#        end
+#    end
+#    end
+#end
 
 # -------------------------------------------------------------
 # BEE operator for boolean
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,14 @@ using BEE
 using Suppressor
 using Test
 
+function capture_render(c)
+    @capture_out render(c)
+end
+
 @testset "BEE.jl" begin
     @testset "simple" begin
+        BEE.reset()
+
         example="""new_int(w, 0, 10)
         new_int(x, 0, 5)
         new_int(z, -5, 10)
@@ -20,14 +26,90 @@ using Test
         ret = @capture_out include("../example/simple-example.jl")
         @test ret == example
 
-        @test "new_bool(x1)\n" == @capture_out render(xl[1])
-
-        @test "new_int(w, 0, 10)\n" == @capture_out render(w)
+        @test "new_int(w, 0, 10)\n" == capture_render(w)
 
         c = x+y == z
-        @test "int_plus(x, y, z)\n" == @capture_out render(c)
+        @test "int_plus(x, y, z)\n" == capture_render(c)
 
         c = xl[1] == -xl[2]
-        @test "bool_eq(x1, -x2)\n" == @capture_out render(c)
+        @test "bool_eq(x1, -x2)\n" == capture_render(c)
+    end
+
+    @testset "Declaring Variable" begin
+        BEE.reset()
+
+        @beebool x1
+        @test "new_bool(x1)\n" == capture_render(x1)
+
+        @beeint i1 3 55
+        @test "new_int(i1, 3, 55)\n" == capture_render(i1)
+    end
+
+    @testset "Boolean statements" begin
+        BEE.reset()
+
+        @beebool x1
+        @beebool x2
+        x3 = BeeBool("x3")
+
+        c = x1 == x2
+        @test "bool_eq(x1, x2)\n" == capture_render(c)
+
+        c = -x1 == x2
+        @test "bool_eq(-x1, x2)\n" == capture_render(c)
+
+        c = true == and([x1, -x2, x3])
+        @test "bool_array_and([x1, -x2, x3])\n" == capture_render(c)
+
+        c = and([x1, x2]) == -x3
+        @test "bool_array_and_reif([x1, x2], -x3)\n" == capture_render(c)
+
+        c = -x3 == BEE.xor(-x1, x2)
+        @test "bool_xor_reif(-x1, x2, -x3)\n" == capture_render(c)
+    end
+
+    @testset "Integer statements" begin
+        BEE.reset()
+
+        @beeint x1 3 7
+        @beeint x2 4 6
+        x3 = beeint("x3", 10, 15)
+
+        c = (x1 < x2) == true
+        @test "int_lt(x1, x2)\n" == capture_render(c)
+
+        c = true == (x1 < x2)
+        @test "int_lt(x1, x2)\n" == capture_render(c)
+
+        c = false == (x1 < x2)
+        @test "int_lt_reif(x1, x2, false)\n" == capture_render(c)
+
+        c = (x1 < x2) == false 
+        @test "int_lt_reif(x1, x2, false)\n" == capture_render(c)
+
+        c = alldiff([x1, x2, x3]) == true
+        @test "int_array_allDiff([x1, x2, x3])\n" == capture_render(c)
+
+        c = true == alldiff([x1, x2, x3])
+        @test "int_array_allDiff([x1, x2, x3])\n" == capture_render(c)
+
+        c = alldiff([x1, x2]) == x3
+        @test "int_array_allDiff_reif([x1, x2], x3)\n" == capture_render(c)
+
+        c = x3 == alldiff([x1, x2])
+        @test "int_array_allDiff_reif([x1, x2], x3)\n" == capture_render(c)
+
+        #c = x1 * x2 == x3
+        #@test "int_times(x1, x2, x3)\n" == capture_render(c)
+
+        #c = x3 == x1 * x2
+        #@test "int_times(x1, x2, x3)\n" == capture_render(c)
+
+        #c = max([x1, x2]) == x3
+        #@test "int_array_max([x1, x2], x3)\n" == capture_render(c)
+
+        #c = x3 == max([x1, x2])
+        #@test "int_array_max([x1, x2], x3)\n" == capture_render(c)
+
     end
 end
